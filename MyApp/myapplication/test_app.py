5fe58957afb3d44a863f0585b5e7f4a3bf172f4e
Gulnara <gulsha@chalmers.se>
2020-12-30 14:57:49 +0100

    Changes are made

--- MyApp/myapplication/test_app.py
+++ MyApp/myapplication/test_app.py
@@ -10,7 +10,7 @@ from knowmine_app import extract_relevant_sentences
 
 def test_extract_relevant_sentences():
     folder_path = os.getcwd() +'\\'
-    main_terms = ["increas", "decreas"]
+    main_terms = ["increas", "predict"]
     relation_words =["toxicit"]
     
     extract_relevant_sentences (folder_path, main_terms, relation_words, "db")     
