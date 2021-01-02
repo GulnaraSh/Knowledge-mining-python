144c8100537402d530116b960771445e67bc95d0
Gulnara <gulsha@chalmers.se>
2021-01-02 17:21:15 +0100

    Update test_FilesReader.py

--- test/test_FilesReader.py
+++ test/test_FilesReader.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Function accessing the files in a user provided folder and returning the list of file names
+Function accessing the files in a user provided folder and
+returning the list of file names
 
 """
 
 
 from knowmine_app.FilesReader import get_file_names
-#import os
+import os
+
 
 def test_get_file_names():
-    
-    pdfFileNames = get_file_names()
-  
-    assert len(pdfFileNames) > 0
-
+    pdffilenames = get_file_names(os.getcwd())
+    assert len(pdffilenames) > 0
